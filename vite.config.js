739ea3119576d1d03d1785f7aa52fb8c80968028
Neha Mehar <nehamehar31@gmail.com>
2025-05-20 22:04:13 +0530

    deploy project

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/generating-pass/',  // ⚠️ This MUST match your repo name
   plugins: [react()],
 })
